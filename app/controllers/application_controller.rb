651fdcb0802dc07b535ac6b14fd9e811f82e146f
James Doyley <jdoyley@gmail.com>
2015-07-14 12:25:42 +0100

    Update search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,24 +8,9 @@ class ApplicationController < ActionController::Base
   before_action :global_site_settings
 
   def index
-    # Yes, this is horrible. Yes, I want to fix it. No, I don't know how to implement componentRestrictions with Geocoder gem.
-    if params[:search_term].present?
-      member_locations = find_location_members("#{params[:search_term]}+United+Kingdom", params[:radius].to_i, cookies[:allowed])
-      @member_locations = member_locations.first.limit(10)
-      @radius = member_locations.last
-      @member_locations_count = member_locations.first
-    elsif params[:town]
-      @member_locations = MemberLocation.where(town: params[:town])
-      @member_locations_count = @member_locations.count
-      @place = params[:town]
-    elsif params[:county]
-      @member_locations = MemberLocation.where(county: params[:county])
-      @member_locations_count = @member_locations.count
-      @place = params[:county]
-    elsif params[:name]
-      @members = Member.name_search(params[:name], cookies[:allowed])
+    unless params.slice(:name, :town, :county, :postcode, :radius).blank?
+      @member_locations, @radius, @member_locations_count = MemberLocationSearch.new(params.slice(:name, :town, :county, :postcode, :radius), cookies[:allowed]).call
     end
-
   end
 
   private
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,25 +1,4 @@
 class Member < ActiveRecord::Base
   has_many :member_locations, dependent: :destroy
   has_one :membership_detail, dependent: :destroy
-
-  def self.name_search(name, show_all)
-    if name.match(/\s/)
-      split_name = name.split(' ')
-      first = split_name.first
-      last = split_name.last
-      search_type = 'AND'
-    else
-      first = last = name
-      search_type = 'OR'
-    end
-    if show_all.present?
-      where(" firstname LIKE :first #{search_type} lastname LIKE :last ", first: "#{first}%", last: "#{last}%").order(:firstname, :lastname)
-    else
-      where(" firstname LIKE :first #{search_type} lastname LIKE :last ", first: "#{first}%", last: "#{last}%").in_practice.order(:firstname, :lastname)
-    end
-  end
-
-  def self.in_practice(limit = nil)
-    joins(:membership_detail).where(membership_details: { in_practice: 'Y', is_admin: 'N' }).limit(limit)
-  end
 end
