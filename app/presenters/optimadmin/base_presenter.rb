3cca0e98de40bc55e8329db9a496ec9f988acc1a
Optimised server <support@eskimosoup.co.uk>
2015-08-06 09:57:19 +0100

    Reipmort functionality

--- app/presenters/optimadmin/base_presenter.rb
+++ app/presenters/optimadmin/base_presenter.rb
@@ -1,6 +1,7 @@
 module Optimadmin
   class BasePresenter
 
+    attr_reader :partial_path
     def initialize(object:, view_template:)
       @object = object
       @view_template = view_template
@@ -12,8 +13,21 @@ module Optimadmin
       end
     end
 
+    # https://robots.thoughtbot.com/rendering-collections-in-rails
+    def to_partial_path
+      @object.to_partial_path
+    end
+
+    def edit_image(image)
+      h.it_edit_images(image).call(@object)
+    end
+
+    def view_link
+      h.link_to eye, h.polymorphic_url(@object), class: 'menu-item-control'
+    end
+
     def edit_link
-      h.link_to pencil, h.polymorphic_url([:edit, @object]), options: { class: 'menu-item-control' }
+      h.link_to pencil, h.polymorphic_url([:edit, @object]), class: 'menu-item-control'
     end
 
     def delete_link
@@ -21,12 +35,12 @@ module Optimadmin
     end
 
     def detail_toggle_link
-      link_to(chevron_down, "#index-list-#{@object.id}", class: 'toggle-module-list-index helper-link', data: { container: "index-list-#{@object.id}", class: 'hide', return: 'true', this_class: 'octicon-chevron-up octicon-chevron-down' }) if can?(:read, @object)
+      h.link_to(chevron_down, "#index-list-#{@object.id}", class: 'toggle-module-list-index helper-link', data: { container: "index-list-#{@object.id}", class: 'hide', return: 'true', this_class: 'octicon-chevron-up octicon-chevron-down' }) # if can?(:read, @object)
     end
 
-    def toggle_link
-      return nil unless @object.respond_to?(:display)
-      link_to((@object.display? ? 'Yes' : 'No'), h.toggle_path(model: @object.class.name.demodulize, id: @object.id, toggle: toggle), id: "display-#{@object.id}", class: "helper-link display #{ @object.display? ? 'true' : 'false' }", remote: true) if can?(:update, @object)
+    def toggle_link(attribute = :display)
+      return nil unless @object.respond_to?(attribute)
+      h.link_to((@object.send("#{attribute}?") ? 'Yes' : 'No'), h.toggle_path(model: @object.class.name.demodulize, id: @object.id, toggle: attribute), id: "#{attribute.to_s}-#{@object.id}", class: "helper-link display #{ @object.send("#{attribute}?") ? 'true' : 'false' }", remote: true)
     end
 
     def show_link
