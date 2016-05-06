687cff2474b43bcbe33094d9b7a8e27d6c1ff0cc
Paul <pauloptimised@users.noreply.github.com>
2016-05-06 12:50:35 +0100

    Fix search specificity

--- app/services/create_search.rb
+++ app/services/create_search.rb
@@ -1,8 +1,7 @@
 class CreateSearch
-
   attr_reader :params, :show_all
   def initialize(params, show_all)
-    @params = params.reject{|k,v| v.blank? }
+    @params = params.reject { |_k, v| v.blank? }
     @show_all = show_all
   end
 
@@ -17,25 +16,32 @@ class CreateSearch
 
   def split_name
     return {} if params[:name].nil?
-    first_name, *middle_names, last_name = params[:name].split(' ')
-    last_name ||= first_name
-    { first_name: first_name, last_name: last_name }
+    # first_name, *middle_names, last_name = params[:name].split(' ')
+    # last_name ||= first_name
+    # { first_name: first_name, last_name: last_name }
+
+    split_name = params[:name].split(' ')
+    first = split_name.first
+    split = params[:name].split(' ')
+    first, *last = split
+    last = last.join(' ')
+
+    { first_name: first, last_name: last }
   end
 
   def split_town_and_county
     return {} if params[:town].nil?
-    dirty_town, dirty_county = params[:town].split("(")
+    dirty_town, dirty_county = params[:town].split('(')
     town = dirty_town.rstrip
-    county = dirty_county.chop #remove closing space
+    county = dirty_county.chop # remove closing space
     { town: town, county: county }
   end
 
   def postcode_and_radius
     return {} if params[:postcode].nil? || params[:radius].nil?
     {
-        postcode: params[:postcode],
-        radius: params[:radius]
+      postcode: params[:postcode],
+      radius: params[:radius]
     }
   end
-
 end
